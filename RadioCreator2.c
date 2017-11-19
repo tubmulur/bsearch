c3cafccaba7c4deb5aa3982ff5697671cddd3b85
Andrey Chekmarev <assminog@gmail.com>
2017-11-19 12:12:38 +0300

    version 0.1

--- RadioCreator2.c
+++ RadioCreator2.c
@@ -4,9 +4,9 @@ int main(int argc, char **argv)
 	{
 	int i, n;
 	char x,ch;
-	
 	n=strlen(argv[1]);
-	for (i=0; i<n; i++)
+	x=argv[1][(n-1)];
+	for (i=0; i<(n-1); i++)
 		{
 		ch=argv[1][i];
 		if(x>ch)
